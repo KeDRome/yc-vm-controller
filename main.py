5781bdfc53aab80d53b596f31caae874ac9940c2
d.kalachev <d.kalachev@avsw.ru>
2023-11-26 13:40:43 +0300

    feat: - дополнен конфиг, - добавлены классы в main.py - CheckRequirements, Monitor, - дополнено руководство

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import logging
+import subprocess
+
+class CheckRequirements():
+    """ Check python3 & OS packages requirements, at running tool """
+    pass
+
+class Monitor():
+    """ Check state of VMs """
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
